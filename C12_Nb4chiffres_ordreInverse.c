d4d8e34e3835fce89c9ed458dd85cf6a728acb71
salma <s.belaalia4677@uca.ac.ma>
2025-09-02 16:06:57 +0100

    Nombre entier à quatre chiffres en ordre inverse (Avec 2 méthodes)

--- C12_Nb4chiffres_ordreInverse.c
+++ C12_Nb4chiffres_ordreInverse.c
@@ -8,11 +8,17 @@ void main()
     scanf("%d",&nombre);
 
     // 1234
-    millier = nombre/1000; // 1
+    /*millier = nombre/1000; // 1
     centaine = (nombre/100) % 10; // 12 -> 2
     dizaine = (nombre/10) % 10; // 123 -> 3
     unite = nombre % 10; // 4
 
-    inverse = unite * 1000 + dizaine * 100 + centaine * 10 + millier;
+    inverse = unite * 1000 + dizaine * 100 + centaine * 10 + millier;*/
+
+    inverse = 0;
+    while(nombre != 0) {
+        inverse = (inverse * 10) + (nombre % 10);
+        nombre = nombre / 10;
+    }
     printf("Le nombre inverse est : %d\n", inverse);
 }
